5492f76c226816c92c7a20c3225f0f5f4de44de0
yefeblgN <64654578+yefeblgn@users.noreply.github.com>
2023-01-18 12:04:52 +0300

    Beipazaru Bot v2.3

---
+++ server.js
@@ -0,0 +1,31 @@
+{
+  "//1": "describes your app and its dependencies",
+  "//2": "https://docs.npmjs.com/files/package.json",
+  "//3": "updating this file will download and update your packages",
+  "name": "hello-express",
+  "version": "0.0.1",
+  "description": "A simple Node app built on Express, instantly up and running.",
+  "main": "heaven.js",
+  "scripts": {
+    "start": "node heaven.js"
+  },
+  "dependencies": {
+    "discord.js": "^11.5.1",
+    "express": "^4.17.1",
+    "fs": "^0.0.2",
+    "ytdl-core": "^2.0.1",
+    "opusscript": "^0.0.7"
+  },
+  "engines": {
+    "node": "8.x"
+  },
+  "repository": {
+    "url": "https://glitch.com/edit/#!/hello-express"
+  },
+  "license": "MIT",
+  "keywords": [
+    "node",
+    "glitch",
+    "express"
+  ]
+}
